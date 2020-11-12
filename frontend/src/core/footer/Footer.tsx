9a6bf2e8ead6096b7e6262eb373342ec03ca24f0
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-12 20:49:32 +0200

    Frontend: Creted a Footer

--- frontend/src/core/footer/Footer.tsx
+++ frontend/src/core/footer/Footer.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
+// import { Link } from "react-router-dom";
 import style from './Footer.module.scss';
 
 export function Footer(): React.ReactElement {
   return (
     <footer>
-      <a href="#" className={style.logo} />
-      <ul className={style['nav-links-center']}>
-        <li className={style['nav-item']}><a href="#">&#169; Lorem ipsum</a></li>
-        <li className={style['nav-item']}><a href="#">Lorem ipsum</a></li>
-        <li className={style['nav-item']}><a href="#">Consectetur</a></li>
-      </ul>
-      <ul className={style['nav-links-right']}>
-        <li className={style['nav-item']}><i className="fab fa-facebook" /></li>
-        <li className={style['nav-item']}><i className="fab fa-instagram" /></li>
-      </ul>
+      <a href="https://www.linkedin.com/in/roberts-ivanovs-3b24b6159/">
+        Made by Roberts Ivanovs
+      </a>
+      <a href="#" >
+        <i className={`${'fab fa-github'} ${style.icon}`} />View project
+      </a>
     </footer>
   );
 }
