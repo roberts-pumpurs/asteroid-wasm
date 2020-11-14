8bcfb4ceaed2cfce589df99ab02b3fd2daf39a33
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-14 23:24:07 +0200

    Backend: Registered some of the `create` veiws to endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,55 +1,60 @@
 import express from 'express';
-import database from './database';
-import { Mouse } from "./models";
+import cors from 'cors';
+import { createCountry, createGame, createUser, getNumNodes, initial } from './database';
+import { Country, Game, User } from './models';
 
 const app = express();
 const port = 8000; // default port to listen
 
-// start the Express server
+/*  ---------- start the Express server ---------- */
 app.listen(port, () => {
   console.log(
-    `server started at http://localhost:${port}\n`
-  + 'Access assets via GET /mouses\n'
-  + 'Access assets via POST /mouses\n'
-  + 'Access assets via GET /mouses/{:id}\n'
-  + 'Access assets via PUT /mouses/{:id}\n'
-  + 'Access assets via DELETE /mouses/{:id}',
+    `server started at http://localhost:${port}`,
   );
+
+  // Perform initial DB setup
+  initial();
 });
-// Parse request body as json
+
 app.use(express.json());
+app.use(cors());
 
-// Get All
-app.get('/api/mouses', (req, res) => {
-  res.send(database.mouse.getAll());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
 });
 
-// GET specific
-app.get('/api/mouses/:id', (req, res) => {
-  const item = database.mouse.get(Number(req.params.id));
-  res.send(item);
-  res.status(item === null ? 404 : 200);
+/* -----------  Neo4j encompassing endpoints ----------- */
+app.get('/api/nodes', async (req, res) => {
+  const nodeCount = await getNumNodes();
+  res.send({ nodeCount });
 });
 
-// Create new
-app.post('/api/mouses', (req, res) => {
-  const obj: Mouse = req.body;
-  const saved = database.mouse.create(obj);
-  res.send({ success: saved });
-  res.status(saved ? 201 : 400);
+app.post('/api/users', async (req, res) => {
+  const obj: User = req.body;
+  const created = await createUser(obj);
+  res.send({ created });
+  res.status(created ? 201 : 400);
 });
 
-// Delete
-app.delete('/api/mouses/:id', (req, res) => {
-  const deleted = database.mouse.delete(Number(req.params.id));
-  res.send({ success: deleted });
-  res.status(deleted ? 204 : 400);
+app.post('/api/countries', async (req, res) => {
+  const obj: Country = req.body;
+  const created = await createCountry(obj);
+  res.send({ created });
+  res.status(created ? 201 : 400);
 });
 
-// Update
-app.put('/api/mouses/:id', (req, res) => {
-  const obj: Mouse = { ...req.body, pk: Number(req.params.id) };
-  const updated = database.mouse.update(obj);
-  res.send({ success: updated });
-  res.status(updated ? 204 : 400);
+interface Params {
+  game: Game,
+  user: User
+}
+app.post('/api/games', async (req, res) => {
+  const obj: Params = req.body;
+  const created = await createGame(obj.game, obj.user);
+  res.send({ created });
+  res.status(created ? 201 : 400);
 });
