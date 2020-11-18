c07fb2e9b732be404212e888499057566f06864e
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-18 12:09:14 +0200

    Frontend: Stars will appear as a background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,19 @@ import { Footer } from 'core/footer/Footer';
 import 'utils/_base.scss';
 import style from 'utils/stars.module.scss';
 
-
 function App(): ReactElement {
   return (
     <>
-      <div>
-        {/* Spawn stars */}
-        {Array.apply(null, Array(100)).map(
-          (_, index) => <div key={index} className={style.star} />,
-        )}
-      </div>
       <header>
         <Header />
       </header>
       <main>
         <Main />
+
+          {/* Spawn stars */}
+          {Array.apply(null, Array(100)).map(
+            (_, index) => <div key={index} className={style.star} />,
+          )}
       </main>
       <footer>
         <Footer />
