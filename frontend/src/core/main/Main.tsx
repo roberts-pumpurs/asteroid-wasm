2a05da3c76a15d550e70657fa4bbaac8db0bc614
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-18 12:13:21 +0200

    Frontend: Leaderboard section

--- frontend/src/core/main/Main.tsx
+++ frontend/src/core/main/Main.tsx
@@ -7,6 +7,7 @@ import {
 import { WasmContextConsumer } from 'WasmContext';
 import { Game } from './Game/Game';
 import { GameHistory } from './GameHistory/GameHistory';
+import { LeaderboardComponent } from './Leaderboard/Leaderboard';
 
 export function Main(): React.ReactElement {
   return (
@@ -20,6 +21,7 @@ export function Main(): React.ReactElement {
           }
         </WasmContextConsumer>
       </Route>
+      <Route exact path="/leaderboards" component={LeaderboardComponent} />
       <Route exact path="/game-history" component={GameHistory} />
     </Switch>
   );
