8276135b4c59f3e61a8f41e6eb29c5e1dca89e3e
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-24 20:51:11 +0200

    Game: Fixed gl setup sometimes causing a crash.
    Handled by looping until everything is fine.

--- frontend/src/rust/src/gl_setup.rs
+++ frontend/src/rust/src/gl_setup.rs
@@ -1,26 +1,36 @@
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
 use web_sys::*;
-use web_sys::{HtmlCanvasElement, Document, WebGlRenderingContext as GL};
+use web_sys::{Document, HtmlCanvasElement, WebGlRenderingContext as GL};
 
-fn wait_until_canvas_is_rendered(document: Document, canvas_id: &str) -> Result<HtmlCanvasElement, Element> {
+fn wait_until_canvas_is_rendered(
+    document: Document,
+    canvas_id: &str,
+) -> Result<HtmlCanvasElement, Element> {
     let canvas: Option<Element> = document.get_element_by_id(canvas_id);
     match canvas {
-        Some(elem) => {
-            elem.dyn_into::<web_sys::HtmlCanvasElement>()
-        }
+        Some(elem) => elem.dyn_into::<web_sys::HtmlCanvasElement>(),
         None => {
             return wait_until_canvas_is_rendered(document, canvas_id);
         }
     }
 }
 
-pub fn initialize_webgl_context(canvas: &HtmlCanvasElement) -> Result<WebGlRenderingContext, JsValue> {
-    let gl: WebGlRenderingContext = canvas.get_context("webgl")?.unwrap().dyn_into()?;
-    gl.clear_color(0.0, 0.0, 0.0, 1.0); //RGBA
-    gl.clear(GL::COLOR_BUFFER_BIT);
-
-    Ok(gl)
+pub fn initialize_webgl_context(
+    canvas: &HtmlCanvasElement,
+) -> Result<WebGlRenderingContext, JsValue> {
+    loop {
+        let gl = canvas.get_context("webgl")?;
+        match gl {
+            Some(gl_inner) => {
+                let gl: WebGlRenderingContext = gl_inner.dyn_into()?;
+                gl.clear_color(0.0, 0.0, 0.0, 1.0); //RGBA
+                gl.clear(GL::COLOR_BUFFER_BIT);
+                return Ok(gl);
+            }
+            _ => (),
+        }
+    }
 }
 
 pub fn get_canvas(canvas_id: &str) -> HtmlCanvasElement {
