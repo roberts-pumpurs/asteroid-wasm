ef0fc10a87801910a579bbe179d2901a1cf7040a
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-25 23:20:15 +0200

    Frontend: Using burger-menu package for navbar

--- frontend/src/core/header/Header.tsx
+++ frontend/src/core/header/Header.tsx
@@ -1,34 +1,86 @@
 import React from 'react';
+import { push as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
 
-import style from './Header.module.scss';
+import sass from './Header.module.scss';
 
+/* inline-JS CSS for react-burger-menu  */
+const styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px',
+  },
+  bmBurgerBars: {
+    background: '#FFFFFF',
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000',
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px',
+  },
+  bmCross: {
+    background: '#bdc3c7',
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%',
+  },
+  bmMenu: {
+    background: '#FFFFFF',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em',
+  },
+  bmMorphShape: {
+    fill: '#373a47',
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em',
+  },
+  bmItem: {
+    display: 'inline-block',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+  },
+};
 export function Header(): React.ReactElement {
   return (
-    <nav>
-      <Link to="/" className={style.logo}> LOGO</Link>
-      <ul className={style['nav-links']}>
-        <li className={style['nav-item']}>
+    <Menu styles={styles} pageWrapId="main" outerContainerId="root">
+      <nav>
+        <span className={sass.logo}>
           <Link to="/">
-            Home
+            ATARI Asteroids
           </Link>
-        </li>
-        <li className={style['nav-item']}>
-          <Link to="/leaderboards">
-            Leaderboards
-          </Link>
-        </li>
-        <li className={style['nav-item']}>
-          <Link to="/game-history">
-            Game History
-          </Link>
-        </li>
-        <li className={style['nav-item']}>
-          <Link to="/about">
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        </span>
+        <ul className={sass['nav-links']}>
+          <li className={sass['nav-item']}>
+            <Link to="/">
+              Home
+            </Link>
+          </li>
+          <li className={sass['nav-item']}>
+            <Link to="/leaderboards">
+              Leaderboards
+            </Link>
+          </li>
+          <li className={sass['nav-item']}>
+            <Link to="/game-history">
+              Game History
+            </Link>
+          </li>
+          <li className={sass['nav-item']}>
+            <Link to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </Menu>
   );
 }
