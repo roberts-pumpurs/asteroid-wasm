263e5745dae251f897ebc6d1e88dbada0ba1d783
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-25 23:58:37 +0200

    Frontend: Game lives shown as hearts

--- frontend/src/core/main/Game/Status.tsx
+++ frontend/src/core/main/Game/Status.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useEffect } from 'react';
 import { GlClient } from 'wasm-app';
 import style from 'core/main/About/About.module.scss';
+import styleG from './Game.module.scss';
 
 interface Props {
   lives: number;
@@ -13,6 +14,9 @@ export function Status({
   score,
   seconds,
 }: Props): ReactElement {
+  const maxLives = 3;
+  const heartsEmpty = Array.apply(null, Array(maxLives - lives)).map((_, index) => <i className={`${'far fa-heart' + ' '}${styleG.heart}`} key={index} />);
+  const heartsFull = Array.apply(null, Array(lives)).map((_, index) => <i className={`${'fas fa-heart' + ' '}${styleG.heart}`} key={index} />);
   return (
     <div className={style['sidebar-options']}>
       <h3>Status</h3>
@@ -20,10 +24,10 @@ export function Status({
         Score: {score}
       </div>
       <div>
-        lives: {lives}
+        seconds: {seconds}
       </div>
       <div>
-        seconds: {seconds}
+        {heartsFull}{heartsEmpty}
       </div>
     </div>
   );
