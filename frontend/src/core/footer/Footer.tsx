3b2a186e73dd5de83355569e5f6b1bdd289b8a87
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-25 10:43:16 +0200

    Frontend: Footer made responsive

--- frontend/src/core/footer/Footer.tsx
+++ frontend/src/core/footer/Footer.tsx
@@ -4,13 +4,13 @@ import style from './Footer.module.scss';
 
 export function Footer(): React.ReactElement {
   return (
-    <>
+    <div className={style['footer-sub']}>
       <a href="https://www.linkedin.com/in/roberts-ivanovs-3b24b6159/">
         Made by Roberts Ivanovs
       </a>
       <a href="#" >
         <i className={`${'fab fa-github'} ${style.icon}`} />View project
       </a>
-    </>
+    </div>
   );
 }
