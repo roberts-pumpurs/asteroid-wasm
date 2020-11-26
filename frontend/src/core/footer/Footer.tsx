bce9aa813b09e2336d29526f783dc7f737d38c2f
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-11-26 22:23:30 +0200

    Frontend: Added links and icons to footer

--- frontend/src/core/footer/Footer.tsx
+++ frontend/src/core/footer/Footer.tsx
@@ -6,9 +6,9 @@ export function Footer(): React.ReactElement {
   return (
     <div className={style['footer-sub']}>
       <a href="https://www.linkedin.com/in/roberts-ivanovs-3b24b6159/">
-        Made by Roberts Ivanovs
+        <i className={`${'fab fa-linkedin'} ${style.icon}`} /> Made by Roberts Ivanovs
       </a>
-      <a href="#" >
+      <a href="https://github.com/roberts-ivanovs/asteroid-wasm">
         <i className={`${'fab fa-github'} ${style.icon}`} />View project
       </a>
     </div>
