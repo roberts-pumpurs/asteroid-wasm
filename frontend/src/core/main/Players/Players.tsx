ce43cef090549892cd818442e27cc1f3452914b2
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-08 18:51:28 +0200

    Frontend: Improved `Player` section
    Now supports user profile editing (local only).

--- frontend/src/core/main/Players/Players.tsx
+++ frontend/src/core/main/Players/Players.tsx
@@ -10,7 +10,11 @@ async function fetchUsers(callback: (arg0: Array<User>) => void): Promise<void> 
 
 export function Players(): ReactElement {
   const [allUsers, setAllUsers] = useState<Array<User>>([]);
-  console.log(style);
+  const [editingUser, setEditingUser] = useState<User | null>(null);
+  const [editingUsername, setEditingUsername] = useState<string | null>(null);
+  const [editingName, setEditingName] = useState<string | null>(null);
+  const [editingSurname, setEditingSurname] = useState<string | null>(null);
+
   useEffect(() => {
     void fetchUsers(setAllUsers);
   }, []);
@@ -22,32 +26,79 @@ export function Players(): ReactElement {
         Players
       </h2>
       <div className={style['user-container']}>
-        {allUsers.map((e) => (
-          <div key={e.username + e.name + e.surname} className={style['user-card']}>
-            <div className={style.header}>
-              {e.username}
+        {allUsers.map((e) => {
+          if (editingUser && e.username === editingUser.username) {
+            return (
+              <div key={e.username + e.name + e.surname} className={style['user-card-editing']}>
+                <div className={style.header}>
+                  <input
+                    value={editingUsername || ''}
+                    onChange={(event) => setEditingUsername(event.target.value)}
+                  />
+                </div>
+                <div className={style.detail}>
+                  <input
+                    value={editingName || ''}
+                    onChange={(event) => setEditingName(event.target.value)}
+                  />
+                  <input
+                    value={editingSurname || ''}
+                    onChange={(event) => setEditingSurname(event.target.value)}
+                  />
+                </div>
+                <div className={style.footer}>
+                  <button
+                    type="button"
+                    className={`${style['btn-tiny']} ${style['draw-border-dark']}`}
+                    onClick={() => { setEditingUser(null); }}
+                  >
+                    Save
+                    <i className="fas fa-user-plus" />
+                  </button>
+                  <button
+                    type="button"
+                    className={`${style['btn-tiny']} ${style['draw-border-dark']}`}
+                  >
+                    Delete
+                    <i className="fas fa-user-slash" />
+                  </button>
+                </div>
+              </div>
+            );
+          }
+          return (
+            <div key={e.username + e.name + e.surname} className={style['user-card']}>
+              <div className={style.header}>
+                {e.username || '[Unknown]'}
+              </div>
+              <div className={style.detail}>
+                {e.name && e.surname ? `${e.name} ${e.surname}` : '[Unknown Unknown]'}
+              </div>
+              <div className={style.footer}>
+                <button
+                  type="button"
+                  className={`${style['btn-tiny']} ${style['draw-border-dark']}`}
+                  onClick={() => {
+                    setEditingUser(e);
+                    setEditingName(e.name);
+                    setEditingUsername(e.username);
+                    setEditingSurname(e.surname);
+                  }}
+                >
+                  Edit
+                  <i className="fas fa-user-edit" />
+                </button>
+                <button
+                  type="button"
+                  className={`${style['btn-tiny']} ${style['draw-border-dark']}`}
+                >
+                  Delete
+                  <i className="fas fa-user-slash" />
+                </button>
+              </div>
             </div>
-            <div className={style.detail}>
-              {e.name} {e.surname}
-            </div>
-            <div className={style.footer}>
-              <button
-                type="button"
-                className={`${style['btn-tiny']} ${style['draw-border-dark']}`}
-              >
-                Edit
-                <i className="fas fa-user-edit" />
-              </button>
-              <button
-                type="button"
-                className={`${style['btn-tiny']} ${style['draw-border-dark']}`}
-              >
-                Delete
-                <i className="fas fa-user-slash" />
-              </button>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
