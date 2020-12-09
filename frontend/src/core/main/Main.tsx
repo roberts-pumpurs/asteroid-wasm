0488d25faa9e4c15e4b6c028b8bb93370906453a
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-09 10:35:50 +0200

    Frontend: Added some animations

--- frontend/src/core/main/Main.tsx
+++ frontend/src/core/main/Main.tsx
@@ -3,38 +3,46 @@ import React from 'react';
 import {
   Switch,
   Route,
+  useLocation,
 } from 'react-router-dom';
 import { WasmContextConsumer } from 'WasmContext';
+import { animated, useTransition } from 'react-spring';
 import { About } from './About/About';
 import { Game } from './Game/Game';
-import { GameHistory } from './Statistics/GameHistory/GameHistory';
-import { LeaderboardComponent } from './Statistics/Leaderboard/Leaderboard';
 import { Statistics } from './Statistics/Statistics';
 import { Players } from './Players/Players';
 
-export function Main(): React.ReactElement {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <WasmContextConsumer>
-          {
-            (context) => context && (
-              <Game wasm={context.wasm} />
-            )
-          }
-        </WasmContextConsumer>
-      </Route>
-      <Route exact path="/statistics" component={Statistics} />
-      <Route exact path="/players" component={Players} />
-      <Route exact path="/about">
-        <WasmContextConsumer>
-          {
-            (context) => context && (
-              <About wasm={context.wasm} />
-            )
-          }
-        </WasmContextConsumer>
-      </Route>
-    </Switch>
-  );
+export function Main(): any {
+  const location = useLocation();
+  const transitions = useTransition(location, (loc) => loc.pathname, {
+    from: { opacity: 1, transform: 'scale(0.5) translateY(-50%)', position: 'relative' },
+    enter: { opacity: 1, transform: 'scale(1) translateY(0)', position: 'relative' },
+    leave: { marginLeft: '10rem', opacity: 0, transform: 'scale(0) translateY(50%)', position: 'absolute' },
+  });
+  return transitions.map(({ item: l, props, key }) => (
+    <animated.div key={key} style={props}>
+      <Switch location={l}>
+        <Route exact path="/">
+          <WasmContextConsumer>
+            {
+              (context) => context && (
+                <Game wasm={context.wasm} />
+              )
+            }
+          </WasmContextConsumer>
+        </Route>
+        <Route exact path="/statistics" component={Statistics} />
+        <Route exact path="/players" component={Players} />
+        <Route exact path="/about">
+          <WasmContextConsumer>
+            {
+              (context) => context && (
+                <About wasm={context.wasm} />
+              )
+            }
+          </WasmContextConsumer>
+        </Route>
+      </Switch>
+    </animated.div>
+  ));
 }
