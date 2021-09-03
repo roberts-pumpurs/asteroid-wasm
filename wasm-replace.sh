c299dee2d2dc8147d4e94c53976d4c1ba150058a
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-09-03 21:12:35 +0300

    ci: install rust

--- wasm-replace.sh
+++ wasm-replace.sh
@@ -1,4 +1,5 @@
-rm -rf node_modules/wasm-app
-mkdir -p node_modules
-mkdir -p node_modules/wasm-app
-cp -r src/rust/pkg/ node_modules/wasm-app
+rm -rf ../../node_modules/wasm-app
+mkdir -p ../../node_modules
+mkdir -p ../../node_modules/wasm-app
+pwd
+cp -r pkg ../../node_modules/wasm-app
